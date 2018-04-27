0c307e030c08d3f314ba02a5835b6dcc04e6cbbc
norbert bautista <norbert.bautista.99@gmail.com>
2018-04-27 16:21:56 +0800

    front end setup 1

---
+++ e-commerce/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,17 @@
+/*active menu*/
+$(function(){
+switch (menu) {
+
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+	
+	default:
+		$('#home').addClass('active');
+		break;
+		
+	}
+});
