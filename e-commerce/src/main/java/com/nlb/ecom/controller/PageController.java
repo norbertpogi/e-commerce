6318f2832d87f9079955c5db1999f842b95b30d8
norbert bautista <norbert.bautista.99@gmail.com>
2018-05-07 19:07:20 +0800

    custom error page global exception

--- e-commerce/src/main/java/com/nlb/ecom/controller/PageController.java
+++ e-commerce/src/main/java/com/nlb/ecom/controller/PageController.java
@@ -1,11 +1,14 @@
 package com.nlb.ecom.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.nlb.ecom.exception.ProductNotFoundException;
 import com.nlb.ecombackend.dao.CategoryDAO;
 import com.nlb.ecombackend.dao.ProductDAO;
 import com.nlb.ecombackend.dto.Category;
@@ -13,6 +16,8 @@ import com.nlb.ecombackend.dto.Product;
 
 @Controller
 public class PageController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
 	
 	@Autowired
 	private CategoryDAO categoryDAO;
@@ -25,6 +30,9 @@ public class PageController {
 
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Home");
+	
+		logger.info("inside pagecontroller index method - INFO");
+		logger.debug("inside pagecontroller index method - DEBUG");
 		
 		//passing the list of categories
 		mv.addObject("categories", categoryDAO.list());
@@ -82,9 +90,11 @@ public class PageController {
 		
 		//viewing a single product
 		@RequestMapping(value = "/show/{id}/product")
-		public ModelAndView showSingleProduct(@PathVariable int id){
+		public ModelAndView showSingleProduct(@PathVariable int id) throws ProductNotFoundException {
 			ModelAndView mv = new ModelAndView("page");
 			Product product = productDAO.get(id);
+			
+			if (product == null) throw new ProductNotFoundException();
 			
 			//update the view count
 			product.setViews(product.getViews() + 1);			
